f056e119cfa206ee382b7df1acae67b2144da911
moha-abdi <mohaa6052@gmail.com>
2024-05-23 14:55:00 +0300

    Remove /ftplugin from .gitignore

---
+++ ftplugin/javascript.lua
@@ -0,0 +1,8 @@
+local opt = vim.opt
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
---
+++ ftplugin/typescript.lua
@@ -0,0 +1,9 @@
+local opt = vim.opt
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+
